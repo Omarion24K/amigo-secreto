444ce0d57335ea41407e30550e580a72a622d001
Omarion24K <vazquznava@gmail.com>
2025-03-19 00:46:27 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if (!nombreAmigo){
+        alert("debes ingresar un nombre");
+        return;
+    }
+     amigo.push(nombreAmigo);
+     inputAmigo.value ="";
+     inputAmigo.focus();
+     console.log (amigo);
+     renderizarAmigo();
+
+}
+
+function renderizarAmigos(){
+    let listaAmigos = document.getElementById(listaAmigos)
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i <amigo.length; i++){
+        let item = document.createElement("li");
+        item.textContent = amigo (i);
+        listaAmigos.appenChild(item)
+
+    }
+
+}
+function sortearAmigo(){
+    if (amigo.length === 0){
+        alert ("No hay amigo para mostrar");
+        return;
+    }
+    let amigoSorteado = amigo[Math.floor(Math.random()* amigo.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+    
+}
+
+
+
+ 
